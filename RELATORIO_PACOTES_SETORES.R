1d03fb6ec88c55b5e6c44ef544f089db0dc51112
unknown <sandro007@gmail.com>
2024-07-18 16:02:50 -0300

    ok

--- RELATORIO_PACOTES_SETORES.R
+++ RELATORIO_PACOTES_SETORES.R
@@ -57,8 +57,35 @@ mutate(`DIAS DE PACOTE ATIVO`= as.numeric((Sys.Date()-PCT_DATA_CADASTRO))) %>%
   
   arrange(desc(PCT_DATA_CADASTRO))
 
-# summary current year ============================================
 
+dev_pacotes <- dbGetQuery(con2,"WITH CLI AS (SELECT DISTINCT C.CLICODIGO,
+                              CLINOMEFANT,
+                               SETOR
+                                FROM CLIEN C
+LEFT JOIN (SELECT CLICODIGO,
+                   E.ZOCODIGO,
+                    ZODESCRICAO SETOR FROM ENDCLI E
+LEFT JOIN (SELECT ZOCODIGO,
+                   ZODESCRICAO FROM 
+                    ZONA WHERE ZOCODIGO IN (20,21,22,23,24,25,26,27,28)
+)Z ON E.ZOCODIGO=Z.ZOCODIGO WHERE ENDFAT='S')A ON C.CLICODIGO=A.CLICODIGO
+ WHERE CLICLIENTE='S')
+
+SELECT P.CLICODIGO,
+        SETOR,
+         PEFDTEMIS EMISSAO,
+          FISCODIGO,
+           ID_PEDIDO,
+            PEFVRTOTAL VRTOTAL FROM PEDFO P
+             INNER JOIN CLI C ON P.CLICODIGO=C.CLICODIGO
+              WHERE PEFSIT<>'C' AND 
+               PEFDTEMIS BETWEEN '01.01.2023' AND 
+                'TODAY' AND 
+                  FISCODIGO IN ('1.94C','2.94C','1.92C','2.92C') AND 
+                   PEFSIT<>'C'")
+
+
+# summary current year ============================================
 
 # Create a sequence of months for the current year
 all_months <- toupper(format(seq(from = as.Date(floor_date(Sys.Date(), "year")), 
@@ -99,11 +126,8 @@ names(pacotes_current_year_with_totals)[ncol(pacotes_current_year_with_totals) -
 # Reorder columns to ensure SETOR is first
 pacotes_current_year_with_totals <- pacotes_current_year_with_totals %>% select(SETOR, everything())
 
-# Print the result
-View(pacotes_current_year_with_totals)
 
-
-## last year ========================================
+## summary last year ========================================
 
 pacotes_last_year <-
   base_pacotes  %>% 
@@ -128,35 +152,62 @@ names(pacotes_last_year_with_totals)[ncol(pacotes_last_year_with_totals) - 1] <-
 # Reorder columns to ensure SETOR is first
 pacotes_last_year_with_totals <- pacotes_last_year_with_totals %>% select(SETOR, everything())
 
-# Print the result
-View(pacotes_last_year_with_totals)
 
 # Align dataframes by SETOR
 merged <- full_join(pacotes_current_year_with_totals, pacotes_last_year_with_totals, by = "SETOR", suffix = c("_current", "_last"))
 
 
+## resumo setores ====================================
 
-# Perform the matrix operation
-varYY <- merged %>% 
-  mutate(across(all_of(paste0(all_months, "_current")), 
-                ~ (.x / merged[[sub("_current", "_last", cur_column())]] - 1), 
+pacotes_year <-
+  rbind(
+    pacotes_current_year %>% mutate(ANO=format(floor_date(Sys.Date(), "year"),"%Y")),
+    pacotes_last_year %>% mutate(ANO=format(floor_date(Sys.Date()-years(1), "year"),"%Y"))) %>% .[,c(1,14,2:13)] %>% 
+  mutate(TOTAL = rowSums(select(., 3:14)))
+
+
+## matrix operation ===============================================
+
+varYY <- merged %>%
+  mutate(across(all_of(c(paste0(all_months, "_current"), "TOTAL_current")),  # added "TOTAL_current"
+                ~ (.x / merged[[sub("_current", "_last", cur_column())]] - 1),
                 .names = "{sub('_current', '', .col)}"))
 
 # Select and reorder the necessary columns
-varYY<- varYY %>% select(SETOR, all_of(all_months)) %>% 
+varYY <- varYY %>% select(SETOR, all_of(c("TOTAL", all_months))) %>%   # added "TOTAL"
   mutate(across(everything(), ~ ifelse(is.nan(.), NA, .))) %>% 
-   mutate(ANO=paste0(format(Sys.Date()-years(1),"%Y")," X ",format(Sys.Date(),"%Y"))) %>% 
-    select(SETOR,ANO,all_of(all_months))
+  mutate(ANO = paste0(format(Sys.Date() - years(1), "%Y"), " X ", format(Sys.Date(), "%Y"))) %>% 
+  select(SETOR, ANO, all_of(c(all_months,"TOTAL")))# added "TOTAL"
 
-## resumo
 
-pacotes_year <-
-rbind(
-pacotes_current_year %>% mutate(ANO=format(floor_date(Sys.Date(), "year"),"%Y")),
-pacotes_last_year %>% mutate(ANO=format(floor_date(Sys.Date()-years(1), "year"),"%Y"))) %>% .[,c(1,14,2:13)] %>% 
-  mutate(TOTAL = rowSums(select(., 3:14)))
-View(pacotes_year)
+## devolução pacotes ===============================================
 
+  
+  # Process the data to ensure all months are included
+dev_pacotes_current_year <-
+  dev_pacotes %>% 
+  
+  filter(floor_date(EMISSAO, "year") == floor_date(Sys.Date(), "year")) %>% 
+  mutate(MES = toupper(format(floor_date(EMISSAO, "month"), "%b")),
+         SETOR = str_extract(SETOR, "SETOR \\d+")) %>% 
+  group_by(MES, SETOR) %>% 
+  summarize(v = sum(VRTOTAL, na.rm = TRUE), .groups = 'drop') %>% 
+  complete(MES = all_months, SETOR = unique(SETOR), fill = list(v = 0)) %>% 
+  mutate(MES = factor(MES, levels = all_months, ordered = TRUE)) %>% 
+  dcast(SETOR ~ MES, value.var = "v") %>% 
+  replace_na(list(JAN = 0, FEV = 0, MAR = 0, ABR = 0, MAI = 0, JUN = 0, JUL = 0, AGO = 0, SET = 0, OUT = 0, NOV = 0, DEZ = 0))
+
+# Add totals row and column
+dev_pacotes_current_year_with_totals <- addmargins(as.matrix(dev_pacotes_current_year[-1]), 1:2)
+dev_pacotes_current_year_with_totals <- as.data.frame(dev_pacotes_current_year_with_totals)
+
+# Adding the SETOR column back and renaming the last column
+dev_pacotes_current_year_with_totals$SETOR <- c(as.character(dev_pacotes_current_year$SETOR), "TOTAL")
+names(dev_pacotes_current_year_with_totals)[ncol(dev_pacotes_current_year_with_totals) - 1] <- "TOTAL" # ncol(cortesias_with_totals) - 1 refers to the last column before adding TOTAL
+
+# Reorder columns to ensure SETOR is first
+dev_pacotes_current_year_with_totals <- dev_pacotes_current_year_with_totals %>% mutate(ANO = paste0(format(Sys.Date(), "%Y"))) %>% 
+  select(SETOR, ANO, all_of(c(all_months,"TOTAL")))# added "TOTAL"
 
 ## create excel setores ==================================================
 
@@ -164,7 +215,7 @@ View(pacotes_year)
 unique_setores <- unique(base_pacotes$SETOR_COMERCIAL)
 
 # Function to create individual workbooks
-create_setores_workbook <- function(setor) {
+   create_setores_workbook <- function(setor) {
   
   # Filter data for the specific client
   setores_data <- base_pacotes[base_pacotes$SETOR_COMERCIAL == setor, ]
@@ -173,10 +224,15 @@ create_setores_workbook <- function(setor) {
   
   setores_data3 <- varYY[varYY$SETOR == str_extract(setor, "SETOR \\d+"), ]
   
+  setores_data4 <- dev_pacotes_current_year_with_totals[dev_pacotes_current_year_with_totals$SETOR == str_extract(setor, "SETOR \\d+"), ]
+  
   pacotes <- createWorkbook()
   
   addWorksheet(pacotes, "RESUMO")
   addWorksheet(pacotes, "BASE")
+  
+  titulo1 <- "RESUMO LANÇAMENTO PACOTES 2023/2024 "
+  titulo2 <- "DEVOLUÇÕES PACOTES"
   
   # Set column widths
   setColWidths(pacotes, sheet = "RESUMO", cols = 1, widths = 1)
@@ -192,22 +248,25 @@ create_setores_workbook <- function(setor) {
   
   percstyle <- createStyle(numFmt = "0.00%")
   
+  datesty <- createStyle(numFmt = "dd/MM/yyyy")
+  
   crescimento <- createStyle(fontColour = "#02862a", bgFill = "#ccf2d8")
   
   queda <- createStyle(fontColour = "#7b1e1e", bgFill = "#e69999")
   
   
+  writeData(pacotes, "RESUMO", titulo1, startCol = 2, startRow = 2)
+  writeData(pacotes, "RESUMO", titulo2, startCol = 2, startRow = 14)
   
-  addStyle(pacotes, sheet = "RESUMO", style = percstyle, cols = c(4:16), rows =11, gridExpand = TRUE)
-  
-  
-  writeDataTable(pacotes, "RESUMO", setores_data2, startCol = 2, startRow = 5, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = FALSE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
+  writeDataTable(pacotes, "RESUMO", setores_data2 %>% mutate(ANO=as.numeric(ANO)), startCol = 2, startRow = 5, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = FALSE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
     
   writeDataTable(pacotes, "RESUMO", setores_data3, startCol = 2, startRow = 10, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = FALSE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
+
+  writeDataTable(pacotes, "RESUMO", setores_data4 %>% mutate(ANO=as.numeric(ANO)), startCol = 2, startRow = 15, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = FALSE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
   
   
   # Apply styles
-  addStyle(pacotes, sheet = "RESUMO", style = numstyle, cols = 4:16, rows = 1:10, gridExpand = TRUE)
+  addStyle(pacotes, sheet = "RESUMO", style = numstyle, cols = 4:16, rows = c(6:17,16), gridExpand = TRUE)
   addStyle(pacotes, sheet = "RESUMO", style = percstyle, cols = 4:16, rows = 11, gridExpand = TRUE)
   
   
@@ -253,12 +312,15 @@ create_setores_workbook <- function(setor) {
   
   setColWidths(pacotes, sheet = "BASE", cols = 16, widths = 20)
   
-  setColWidths(pacotes, sheet = "BASE", cols = 17, widths = 20)  
+  setColWidths(pacotes, sheet = "BASE", cols = 17, widths = 20) 
+  
+  setColWidths(pacotes, sheet = "BASE", cols = 19, widths = 20) 
   
   writeDataTable(pacotes, "BASE", setores_data, startCol = 1, startRow = 1, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = TRUE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
   
   addStyle(pacotes, sheet = "BASE", style = numstyle, cols = c(9,19), rows = 1:nrow(setores_data)+1, gridExpand = TRUE)
-
+  
+  addStyle(pacotes, sheet = "BASE", style = datesty, cols = c(5,6,17), rows = 1:nrow(setores_data)+1, gridExpand = TRUE)
   
   setor_prefix <- sub("^(\\D*)(\\d+).*", "\\1_\\2", setor)
   
@@ -278,6 +340,9 @@ pacotes_geral <- createWorkbook()
 addWorksheet(pacotes_geral, "RESUMO")
 addWorksheet(pacotes_geral, "BASE")
 
+titulo1 <- "RESUMO LANÇAMENTO PACOTES 2023/2024 "
+titulo2 <- "DEVOLUÇÕES PACOTES"
+
 # Set column widths
 setColWidths(pacotes_geral, sheet = "RESUMO", cols = 1, widths = 1)
 
@@ -292,6 +357,8 @@ numstyle <- createStyle(numFmt = "#,##0")
 
 percstyle <- createStyle(numFmt = "0.00%")
 
+datesty <- createStyle(numFmt = "dd/MM/yyyy")
+
 crescimento <- createStyle(fontColour = "#02862a", bgFill = "#ccf2d8")
 
 queda <- createStyle(fontColour = "#7b1e1e", bgFill = "#e69999")
@@ -300,6 +367,7 @@ queda <- createStyle(fontColour = "#7b1e1e", bgFill = "#e69999")
 # Apply styles
 addStyle(pacotes_geral, sheet = "RESUMO", style = numstyle, cols = 4:16, rows = 1:24, gridExpand = TRUE)
 addStyle(pacotes_geral, sheet = "RESUMO", style = percstyle, cols = 4:16, rows = 28:36, gridExpand = TRUE)
+addStyle(pacotes_geral, sheet = "RESUMO", style = numstyle, cols = 4:16, rows = 41:48, gridExpand = TRUE)
 
 
 crescimento <- createStyle(fontColour = "#02862a", bgFill = "#ccf2d8")
@@ -311,10 +379,12 @@ queda <- createStyle(fontColour = "#7b1e1e", bgFill = "#e69999")
 conditionalFormatting(pacotes_geral, sheet = "RESUMO", cols = 4:16, rows = 28:36, rule = '> 0', style = crescimento)
 conditionalFormatting(pacotes_geral, sheet = "RESUMO", cols = 4:16, rows = 28:36, rule = '< 0', style = queda)
 
-
+writeData(pacotes_geral, "RESUMO", titulo1, startCol = 2, startRow = 1)
+writeData(pacotes_geral, "RESUMO", titulo2, startCol = 2, startRow = 39)
 writeDataTable(pacotes_geral, "RESUMO", pacotes_current_year_with_totals %>% mutate(ANO=as.numeric(format(floor_date(Sys.Date(), "year"),"%Y"))) %>%  select(1, ANO, everything()), startCol = 2, startRow = 3, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = FALSE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
 writeDataTable(pacotes_geral, "RESUMO", pacotes_last_year_with_totals %>% mutate(ANO=as.numeric(format(floor_date(Sys.Date()-years(1), "year"),"%Y"))) %>%  select(1, ANO, everything()), startCol = 2, startRow = 15, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = FALSE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
 writeDataTable(pacotes_geral, "RESUMO", varYY, startCol = 2, startRow = 27, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = FALSE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
+writeDataTable(pacotes_geral, "RESUMO", dev_pacotes_current_year_with_totals %>%  mutate(ANO=as.numeric(format(floor_date(Sys.Date(), "year"),"%Y"))), startCol = 2, startRow = 40, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = FALSE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
 
 
 # Set column widths
@@ -352,10 +422,20 @@ setColWidths(pacotes_geral, sheet = "BASE", cols = 16, widths = 20)
 
 setColWidths(pacotes_geral, sheet = "BASE", cols = 17, widths = 20)  
 
+setColWidths(pacotes_geral, sheet = "BASE", cols = 19, widths = 20) 
+
 writeDataTable(pacotes_geral, "BASE", base_pacotes, startCol = 1, startRow = 1, tableStyle = "TableStyleMedium2",tableName = NULL, headerStyle = NULL, withFilter = TRUE, keepNA = FALSE, na.string = NULL, sep = ", ", stack = FALSE, firstColumn = FALSE, lastColumn = FALSE, bandedRows = TRUE, bandedCols = FALSE)
 
 addStyle(pacotes_geral, sheet = "BASE", style = numstyle, cols = c(9,19), rows = 1:nrow(base_pacotes)+1, gridExpand = TRUE)
 
+addStyle(pacotes_geral, sheet = "BASE", style = datesty, cols = c(5,6,17), rows = 1:nrow(base_pacotes)+1, gridExpand = TRUE)
+
 file_path2 <- paste0("C:\\Users\\REPRO SANDRO\\OneDrive - Luxottica Group S.p.A (1)\\PACOTES\\PACOTES_GERAL.xlsx")
 
 saveWorkbook(pacotes_geral, file = file_path2, overwrite = TRUE)
+
+
+
+
+
+
